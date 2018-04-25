4c7f89e0edb36bc8cf67760bb2dceb52887ab88d
joanlojo <joanlojo.7@gmail.com>
2018-04-25 03:03:44 +0200

    salto y movimeinto

--- Unity_2D_MecanicasAvanzadas/Assets/Scripts/Plataforma/movimientoPelota.cs
+++ Unity_2D_MecanicasAvanzadas/Assets/Scripts/Plataforma/movimientoPelota.cs
@@ -4,27 +4,72 @@ using UnityEngine;
 
 public class movimientoPelota : MonoBehaviour {
 
-    public float speed;
-    public float salto;
+    public bool jump = false;
+
+    public float Force = 365f;
+    public float salto = 1000f;
+    public float speed = 5f;
+    bool doublejump;
+    //float distGround;
     private Rigidbody2D rb;
+    int jumps;
+    public int maxjumps = 2;
+    private bool isGrounded;
     // Use this for initialization
     void Start()
     {
-       // salto = 0;
+        doublejump = false;
+        isGrounded = true;
+        //distGround = Vector2.Distance(transform.position, collider2D.transform.position);
         rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        Vector2 movement = new Vector2(1.0f, 0.0f);
 
-        Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
-
-
-        if (Input.GetKeyDown(KeyCode.Space)){
-            movement = new Vector3(speed, salto, 0.0f);
+        if (movement.x * rb.velocity.x < speed)
+        {
+            rb.AddForce(Vector2.right * movement.x * Force);
         }
+        if (Mathf.Abs(rb.velocity.x) > speed)
+        {
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * speed, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            Jump();
 
-        rb.velocity = (movement * speed);
+        }
     }
+
+    void Jump() { 
+   
+        if (jumps > 0)
+        {
+            rb.velocity = new Vector2(0f, salto);
+            isGrounded = false;
+            jumps = jumps - 1;
+        }
+        if (jumps == 0)
+        {
+            return;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collide)
+    {
+        if (collide.gameObject.tag == "Suelo")
+        {
+            jumps = maxjumps;
+            isGrounded = true;
+           // movespeed = 2.0F;
+
+        }
+    }
+   /*         void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isGrounded = true;
+        }
+    }*/
 }
